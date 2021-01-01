a46b7ddf82e9bdf232288b7b4936116412f2eee0
merik <merikbest2015@gmail.com>
2021-01-01 22:48:41 +0300

    Added Review template to Perfume page.

--- frontend/src/pages/Perfume/Perfume.js
+++ frontend/src/pages/Perfume/Perfume.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCartPlus} from "@fortawesome/free-solid-svg-icons";
+import {faCartPlus, faPaperPlane} from "@fortawesome/free-solid-svg-icons";
 
 import {IMG_URL} from "../../utils/constants/url";
 import {fetchPerfume} from "../../actions/perfume-actions";
@@ -28,75 +28,136 @@ class Perfume extends Component {
         const {perfume} = this.props;
 
         return (
-            <div>
-                <div className="container mt-5 pb-5">
-                    <div className="row">
-                        <div className="col-md-5">
-                            <div>
-                                <img src={IMG_URL + `${perfume.filename}`}
-                                     className="rounded mx-auto w-100"/>
-                            </div>
-                        </div>
-                        <div className="col-md-7">
-                            <h2>{perfume.perfumeTitle}</h2>
-                            <h3>{perfume.perfumer}</h3>
-                            <p>Product code: <span>{perfume.id}</span></p>
-                            <p style={{color: "#54C0A1"}}>In Stock</p>
-                            <div className="row ml-1">
-                                <h6 className="mr-5"><span>${perfume.price}</span>.00</h6>
-                                <button type="submit"
-                                        className="btn btn-success mx-3"
-                                        onClick={this.addToCart}>
-                                    <FontAwesomeIcon className="mr-2 fa-lg" icon={faCartPlus}/> ADD TO CART
-                                </button>
-                            </div>
-                            <br/>
-                            <table className="table">
-                                <tbody>
-                                <tr>
-                                    <td>Perfume title:</td>
-                                    <td>{perfume.perfumeTitle}</td>
-                                </tr>
-                                <tr>
-                                    <td>Brand:</td>
-                                    <td>{perfume.perfumer}</td>
-                                </tr>
-                                <tr>
-                                    <td>Perfume type:</td>
-                                    <td>{perfume.type}</td>
-                                </tr>
-                                <tr>
-                                    <td>Release year:</td>
-                                    <td>{perfume.year}</td>
-                                </tr>
-                                <tr>
-                                    <td>Volume:</td>
-                                    <td><span>{perfume.volume}</span> ml.</td>
-                                </tr>
-                                <tr>
-                                    <td>Manufacturer country:</td>
-                                    <td>{perfume.country}</td>
-                                </tr>
-                                <tr>
-                                    <td>Gender:</td>
-                                    <td>{perfume.perfumeGender}</td>
-                                </tr>
-                                <tr>
-                                    <td>Top notes:</td>
-                                    <td>{perfume.fragranceTopNotes}</td>
-                                </tr>
-                                <tr>
-                                    <td>Heart notes:</td>
-                                    <td>{perfume.fragranceMiddleNotes}</td>
-                                </tr>
-                                <tr>
-                                    <td>Base notes:</td>
-                                    <td>{perfume.fragranceBaseNotes}</td>
-                                </tr>
-                                </tbody>
-                            </table>
+            <div className="container mt-5 pb-5">
+                <div className="row">
+                    <div className="col-md-5">
+                        <div>
+                            <img src={IMG_URL + `${perfume.filename}`}
+                                 className="rounded mx-auto w-100"/>
                         </div>
                     </div>
+                    <div className="col-md-7">
+                        <h2>{perfume.perfumeTitle}</h2>
+                        <h3>{perfume.perfumer}</h3>
+                        <p>Product code: <span>{perfume.id}</span></p>
+                        <p style={{color: "#54C0A1"}}>In Stock</p>
+                        <div className="row ml-1">
+                            <h6 className="mr-5"><span>${perfume.price}</span>.00</h6>
+                            <button type="submit"
+                                    className="btn btn-success mx-3"
+                                    onClick={this.addToCart}>
+                                <FontAwesomeIcon className="mr-2 fa-lg" icon={faCartPlus}/> ADD TO CART
+                            </button>
+                        </div>
+                        <br/>
+                        <table className="table">
+                            <tbody>
+                            <tr>
+                                <td>Perfume title:</td>
+                                <td>{perfume.perfumeTitle}</td>
+                            </tr>
+                            <tr>
+                                <td>Brand:</td>
+                                <td>{perfume.perfumer}</td>
+                            </tr>
+                            <tr>
+                                <td>Perfume type:</td>
+                                <td>{perfume.type}</td>
+                            </tr>
+                            <tr>
+                                <td>Release year:</td>
+                                <td>{perfume.year}</td>
+                            </tr>
+                            <tr>
+                                <td>Volume:</td>
+                                <td><span>{perfume.volume}</span> ml.</td>
+                            </tr>
+                            <tr>
+                                <td>Manufacturer country:</td>
+                                <td>{perfume.country}</td>
+                            </tr>
+                            <tr>
+                                <td>Gender:</td>
+                                <td>{perfume.perfumeGender}</td>
+                            </tr>
+                            <tr>
+                                <td>Top notes:</td>
+                                <td>{perfume.fragranceTopNotes}</td>
+                            </tr>
+                            <tr>
+                                <td>Heart notes:</td>
+                                <td>{perfume.fragranceMiddleNotes}</td>
+                            </tr>
+                            <tr>
+                                <td>Base notes:</td>
+                                <td>{perfume.fragranceBaseNotes}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div className="mt-5">
+                    <h3 className="text-center mb-5"> REVIEWS </h3>
+
+                    <form>
+                        <div className="form-group border">
+                            <div className="mx-3 my-3">
+                                <label htmlFor="exampleFormControlInput1">
+                                    <span className="text-danger"><b>*</b></span> Your name
+                                </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="exampleFormControlInput1"/>
+                                <label htmlFor="exampleFormControlTextarea1">
+                                    <span className="text-danger"><b>*</b></span> Message text
+                                </label>
+                                <textarea
+                                    className="form-control"
+                                    id="exampleFormControlTextarea1"
+                                    rows="4"
+                                    style={{resize: "none"}}/>
+                                <button type="submit" className="btn btn-dark mt-3">
+                                    <FontAwesomeIcon className="mr-2" icon={faPaperPlane}/>Post a review
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+
+                    <div className="form row mt-5">
+                        <div className="col-3">
+                            <p><b>John Doe</b></p>
+                            <p>01.01.12</p>
+                        </div>
+                        <div className="col">
+                            <p>Wow! this is really great.</p>
+                        </div>
+                    </div>
+                    <hr/>
+
+                    <div className="form row mt-5">
+                        <div className="col-3">
+                            <p><b>John Doe</b></p>
+                            <p>01.01.12</p>
+                        </div>
+                        <div className="col">
+                            <p>Wow! this is really great.</p>
+                        </div>
+                    </div>
+                    <hr/>
+
+                    <div className="form row mt-5">
+                        <div className="col-3">
+                            <p><b>John Doe</b></p>
+                            <p>01.01.12</p>
+                        </div>
+                        <div className="col">
+                            <p>Wow! this is really great.</p>
+                        </div>
+                    </div>
+                    <hr/>
                 </div>
             </div>
         );
